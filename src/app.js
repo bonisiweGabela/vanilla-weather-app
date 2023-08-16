59d2e285b558ca2c71393e68b4fd0d0df793251d
bonisiweGabela <142170923+bonisiweGabela@users.noreply.github.com>
2023-08-16 17:42:27 +0200

    Edited java script

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours;
+  let day = date.getDay;
+  return `${day} ${hours}:${minuntes}`;
+  let minuntes = date.getMinutes;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
   windElement.innerHTML = Math.round(response.data.wind.speed);
   humidityElement.innerHTML = response.data.main.humidity;
   descriptionElement.innerHTML = response.data.weather[0].description;
   cityElement.innerHTML = response.data.name;
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "b37ef2363439c504480df8d372acfaa0";
