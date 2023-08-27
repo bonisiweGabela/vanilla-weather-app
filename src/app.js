4067dca75863c829db634e58f34a23b64b1b055b
bonisiweGabela <142170923+bonisiweGabela@users.noreply.github.com>
2023-08-27 20:19:25 +0200

    Updated API key

--- src/app.js
+++ src/app.js
@@ -68,8 +68,9 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 function getForecast(coordinates) {
-  let apiKey = "b37ef2363439c504480df8d372acfaa0";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiKey = "8cd9be374c7c96c39a9fe73f4bf2f055";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
   axios.get(apiUrl).then(displayForecast);
 }
 function displayTemperature(response) {
